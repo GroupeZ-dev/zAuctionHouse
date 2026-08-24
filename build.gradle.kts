d494aa486a9af15f6c9a08868740b82ae8a0382a
Maxence <simon.maxence@hotmail.com>
2026-08-25 00:48:05 +0200

    :memo: Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "fr.maxlego08.zauctionhouse"
-version = "4.0.1.2"
+version = "4.0.1.3"
 
 extra.set("targetFolder", file("target/"))
 extra.set("apiFolder", file("target-api/"))
